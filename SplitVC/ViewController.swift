59ee60070612e58ead38a380a2dec0acc0b2a566
Ruslan Filistovich <ruslan.filistovich@intexsoft.by>
2020-02-26 16:44:47 +0300

    Initial Commit

---
+++ SplitVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitVC
+//
+//  Created by Ruslan Filistovich on 26/02/2020.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
