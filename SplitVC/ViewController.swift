c6f6b88397cd86ec2640fbb514a6410c9f688d8c
Ruslan Filistovich <ruslan.filistovich@intexsoft.by>
2020-02-26 17:12:08 +0300

    setting split vc

--- SplitVC/ViewController.swift
+++ SplitVC/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MasterController: UITableViewController {
+
+    override func viewDidLoad() {
+        self.tableView = UITableView(frame: .zero, style: .plain)
+        super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "id")
+        self.navigationItem.title = "Master"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "id")!
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       let controller = DetailController()
+       controller.navigationTitle = "\(indexPath)"
+       self.showDetailViewController(UINavigationController(rootViewController: controller), sender: nil)
+    }
+}
+
+class DetailController: UIViewController {
+
+    var navigationTitle: String = "Detail"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = navigationTitle
+        self.view.backgroundColor = .white
     }
-
-
 }
-
